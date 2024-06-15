1c09e28e9c2dd5df99715dc49e91973bcfc85576
hildhild <huy.ledinh@hcmut.edu.vn>
2024-06-15 19:21:02 +0700

    complete pickedbyheader, monthlybestseller

--- index.js
+++ index.js
@@ -1,134 +1,159 @@
 function translateX10px(index) {
-    var effectedElement = document.getElementsByClassName('translateX10px');
-    // Thực hiện bất kỳ thay đổi CSS nào bạn muốn ở đây
-    effectedElement[index].style.transform = 'translateX(10px)';
+    var translateX10px = document.getElementsByClassName('translateX10px');
+    translateX10px[index].style.transform = 'translateX(10px)';
 }
 
 function resetTransform(index) {
-    var effectedElements = document.getElementsByClassName('translateX10px');
-    effectedElements[index].style.transform = 'none';
+    var translateX10px = document.getElementsByClassName('translateX10px');
+    translateX10px[index].style.transform = 'none';
 }
 
 function mouseEnterBook(index) {
-    var effectedElements = document.getElementsByClassName('book-img');
-    var listButtonsBook = document.getElementsByClassName('list-buttons-book');
+    var bookImg = document.getElementsByClassName(`book-img-${index}`);
+    var listButtonsBook = document.getElementsByClassName(`list-buttons-book-${index}`);
     switch (index) {
         case 0: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-2.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-2.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
             break;
         }
         case 1: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-29.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-29.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
             break;
         }
         case 2: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-1.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-1.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
             break;
         }
         case 3: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-118.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-118.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
             break;
         }
         case 4: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-109.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-109.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
             break;
         }
         case 5: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-117.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-116.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
             break;
         }
         case 6: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-109.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-117.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
+            break;
+        }
+        case 7: {
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-109.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
+            break;
+        }
+        case 8: {
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-5.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
             break;
         }
         default: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-5.jpg'; 
-            listButtonsBook[index].style.opacity = '1';
-            listButtonsBook[index].style.transform = 'translateY(-50px)';
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-2.jpg'; 
+            listButtonsBook[0].style.opacity = '1';
+            listButtonsBook[0].style.transform = 'translateY(-50px)';
         break;
         }
     }
 }
 
 function mouseLeaveBook(index) {
-    var effectedElements = document.getElementsByClassName('book-img');
-    var listButtonsBook = document.getElementsByClassName('list-buttons-book');
+    var bookImg = document.getElementsByClassName(`book-img-${index}`);
+    var listButtonsBook = document.getElementsByClassName(`list-buttons-book-${index}`);
     switch (index) {
         case 0: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-1.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-1.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
         case 1: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-109.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-109.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
         case 2: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-4.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-4.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
         case 3: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-22.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-22.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
         case 4: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-14.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/04/product-14.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
         case 5: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-3.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-23.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
         case 6: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-98.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-3.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
+            break;
+        }
+        case 7: {
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-98.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
+            break;
+        }
+        case 8: {
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-2.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
         default: {
-            effectedElements[index].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2019/04/product-2.jpg'; 
-            listButtonsBook[index].style.transform = 'translateY(0)';
-            listButtonsBook[index].style.opacity = 0;
+            bookImg[0].src = 'https://wpbingosite.com/wordpress/oriobook/wp-content/uploads/2020/12/product-76.jpg'; 
+            listButtonsBook[0].style.transform = 'translateY(0)';
+            listButtonsBook[0].style.opacity = 0;
             break;
         }
     }
 }
 
 function mouseEnterPicture(index) {
-    var effectedElements = document.getElementsByClassName('hover-picture-bg');
+    var hoverPictureBg = document.getElementsByClassName('hover-picture-bg');
     var picture = document.getElementsByClassName('picture-img');
-    effectedElements[index].style.display = "block";
+    hoverPictureBg[index].style.display = "block";
     picture[index].style.transform = "scale(1.2)";
 }
 
 function mouseLeavePicture(index) {
-    var effectedElements = document.getElementsByClassName('hover-picture-bg');
+    var hoverPictureBg = document.getElementsByClassName('hover-picture-bg');
     var picture = document.getElementsByClassName('picture-img');
-    effectedElements[index].style.display = "none";
+    hoverPictureBg[index].style.display = "none";
     picture[index].style.transform = "none";
 }
+
+
